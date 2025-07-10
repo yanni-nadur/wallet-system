d7a3e3c7ffe8b4ce84cfcdf1cbc9f579621c5bed
Yanni Nadur <yanninadur@gmail.com>
2025-07-10 01:23:09 -0300

    chore: implementing global error filter on main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Use global exception filter
+  app.useGlobalFilters(new AllExceptionsFilter());
+  
+  await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
